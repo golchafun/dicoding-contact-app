f56d79b3dd94d999ada2120aff51036a1a38b1b1
golchafun <wannabe.golcha@gmail.com>
2025-06-16 14:53:08 +0700

    add search bar using query parameter url

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import ContactList from '../components/ContactList';
 import { deleteContact, getContacts } from '../utils/data';
+import SearchBar from '../components/SearchBar';
+import { useSearchParams } from 'react-router-dom';
+
+function HomePageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const keyword = searchParams.get('keyword');
+  function changeSearchParams(keyword) {
+    setSearchParams({ keyword });
+  }
+ 
+  return <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />
+}
  
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
  
     this.state = {
-      contacts: getContacts()
+      contacts: getContacts(),
+      keyword: props.defaultKeyword || '',
     }
  
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
   }
  
+  onKeywordChangeHandler(keyword) {
+    this.setState(() => {
+      return {
+        keyword,
+      }
+    });
+    this.props.keywordChange(keyword);
+  }
+
   onDeleteHandler(id) {
     deleteContact(id);
  
@@ -25,13 +48,20 @@ class HomePage extends React.Component {
   }
  
   render() {
+    const contacts = this.state.contacts.filter((contact) => {
+      return contact.name.toLowerCase().includes(
+        this.state.keyword.toLowerCase()
+      );
+    });
+
     return (
       <section>
+        <SearchBar keyword={this.state.keyword} keywordChange={this.onKeywordChangeHandler} />
         <h2>Daftar Kontak</h2>
-        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
+        <ContactList contacts={contacts} onDelete={this.onDeleteHandler} />
       </section>
     )
   }
 }
  
-export default HomePage;
+export default HomePageWrapper;
