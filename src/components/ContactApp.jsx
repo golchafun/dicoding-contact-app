3e4eebdc514bfa54b0550237a462d57131ac1216
golchafun <wannabe.golcha@gmail.com>
2025-06-20 16:39:46 +0700

    add register, login, and logout features

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -3,6 +3,9 @@ import { Route, Routes } from 'react-router-dom';
 import Navigation from './Navigation';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
+import RegisterPage from '../pages/RegisterPage';
+import LoginPage from '../pages/LoginPage';
+import { getUserLogged, putAccessToken } from '../utils/api';
 
 class ContactApp extends React.Component {
   constructor(props) {
@@ -10,19 +13,56 @@ class ContactApp extends React.Component {
 
     this.state = {
       authedUser: null,
+      initializing: true,
     };
+    this.onLoginSuccess = this.onLoginSuccess.bind(this);
+    this.onLogout = this.onLogout.bind(this);
+  }
+
+  onLogout() {
+    this.setState(() => {
+      return {
+        authedUser: null
+      }
+    });
+    putAccessToken('');
+  }
+
+  async componentDidMount() {
+    const { data } = await getUserLogged();
+    this.setState(() => {
+      return {
+        authedUser: data,
+        initializing: false
+      }
+    });
+  }
+
+  async onLoginSuccess({ accessToken }) {
+    putAccessToken(accessToken);
+    const { data } = await getUserLogged();
+    this.setState(() => {
+      return {
+        authedUser: data,
+      };
+    });
   }
   render() {
+    if (this.state.initializing) {
+      return null;
+    }
+
     if (this.state.authedUser === null) {
       return (
         <div className='contact-app'>
           <header className='contact-app__header'>
             <h1>Aplikasi Kontak</h1>
+           
           </header>
           <main>
             <Routes>
-              <Route path="/*" element={<p>Halaman Login</p>} />
-              <Route path="/register" element={<p>Halaman Register</p>} />
+              <Route path="/*" element={<LoginPage loginSuccess={this.onLoginSuccess} />} />
+              <Route path="/register" element={<RegisterPage />} />
             </Routes>
           </main>
         </div>
@@ -32,7 +72,7 @@ class ContactApp extends React.Component {
       <div className="contact-app">
         <header className='contact-app__header'>
           <h1>Aplikasi Kontak</h1>
-          <Navigation />
+           <Navigation logout={this.onLogout} name={this.state.authedUser.name} />
         </header>
         <main>
           <Routes>
